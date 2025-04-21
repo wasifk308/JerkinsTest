7de5f848727d75b9075a57e338643a47222bb484
wasifk308 <wasifk308@gmail.com>
2025-04-21 12:37:51 +0530

    Init
    init

---
+++ helo.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class helo
+{
+        public static void main(String[] args) {
+
+            System.out.println();
+            System.out.println("hello world..");    
+            
+            System.out.println(new Date());        
+
+        }
+}   
