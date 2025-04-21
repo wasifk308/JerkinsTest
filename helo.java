1f297387195065bea7d85df912e69328d41f0895
wasifk308 <wasifk308@gmail.com>
2025-04-21 12:50:23 +0530

    update

--- helo.java
+++ helo.java
@@ -5,7 +5,7 @@ public class helo
         public static void main(String[] args) {
 
             System.out.println();
-            System.out.println("hello world...");    
+            System.out.println("hello world....");    
             
             System.out.println(new Date());        
 
